de70771d66da74e6bea81e649e011672888c4b05
Billy Shen <billy.shen@hetic.net>
2016-02-10 09:19:28 +0100

    [ADD] Song, Tag and User controllers + views

---
+++ app/scripts/controllers/song.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jaiyeApp.controller:SongCtrl
+ * @description
+ * # SongCtrl
+ * Controller of the jaiyeApp
+ */
+angular.module('jaiyeApp')
+  .controller('SongCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/tag.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jaiyeApp.controller:TagCtrl
+ * @description
+ * # TagCtrl
+ * Controller of the jaiyeApp
+ */
+angular.module('jaiyeApp')
+  .controller('TagCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
